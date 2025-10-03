bcd2a460e51250c59b07da53292a26ac0125a0e7
Jonathan P <jonathanphung2005@gmail.com>
2025-10-03 13:56:54 -0700

    working on organizer cpp

--- Organizer.cpp
+++ Organizer.cpp
@@ -1,6 +1,25 @@
 // TO DO: #include needed standard libraries and your own libraries here
+#include <iostream>
+#include <string>
+#include "Organizer.h"
+
+using std::cout;
+using std::endl;
 
 // TO DO: function implementations
+void Orgainizer::displayProfile() const{
+	cout<< "Username: " << username << endl
+	<<"Email: " << email << endl
+	<<"Bio: " << bio << endl
+	<<"Profile Picture: "<< profilePicture << endl
+
+}
+
+bool Organizer :: changePassword () const{
+	if(newPW.empty()) return false;
+	password = newPw;
+	return true
+}
 
 // Operator == overloading implementation
 bool Organizer::operator==(const Organizer& otherOrganizer) const {
